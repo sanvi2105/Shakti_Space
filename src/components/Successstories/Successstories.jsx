1f5a5518bcd6a9f5f2cbf7686e0f46401fc7c4bf
sanvi2105 <sanvisagar145@gmail.com>
2026-03-20 18:11:37 +0530

    handled bug

--- src/components/Successstories/Successstories.jsx
+++ src/components/Successstories/Successstories.jsx
@@ -340,7 +340,7 @@ const SuccessStories = () => {
         </div>
       )}
 
-      {/* ✅ SAME GRID (BUT USING allStories) */}
+      {/*  SAME GRID (BUT USING allStories) */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 auto-rows-min grid-flow-dense">
         {allStories.map((story, index) => (
           <motion.div
