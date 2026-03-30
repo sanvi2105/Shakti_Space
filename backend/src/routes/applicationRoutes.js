afddbd9c7eea88dd205c126d1380d610a739330d
sanvi2105 <sanvisagar145@gmail.com>
2026-03-30 23:05:57 +0530

    updated applicationRoutes

--- backend/src/routes/applicationRoutes.js
+++ backend/src/routes/applicationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import protect from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 import {
   applyJob,
   getUserApplication,
